9723ec3de392dd1bf764d963fc1a8a9cbbc12f86
Milan Panta <milan@neptunemutual.com>
2022-01-19 15:03:53 +0545

    chore: update tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    boxShadow: {
+      supportCard: "0px 29.6346px 78.5962px rgba(74, 74, 74, 0.5)",
+      powerCard: "0px 4px 14px rgba(0, 0, 0, 0.1)",
+      brand: "0px 7px 22px rgba(0, 0, 0, 0.05)"
+    },
+    fontSize: {
+      h1: ["76px", "104px"],
+      h2: ["61px", "72px"],
+      h3: ["49px", "64px"],
+    },
+    fontFamily: {
+      poppins: "Poppins, sans-serif",
+      sora: "Sora, sans-serif",
+      inter: "Inter, sans-serif",
+    },
+    extend: {
+      color: {
+        "010311": "#010311",
+        "333333": "#333333",
+        "01052D": "#01052D",
+        "4E7DD9": "#4E7DD9",
+        "9A9A9A": "#9A9A9A",
+        "b0c4db80": "#b0c4db80",
+        "3C69FE": "#3C69FE",
+        "E86BFB": "#E86BFB",
+        "4289F2": "#4289F2",
+        "FA5C2F": "#FA5C2F",
+        "A44ED9": "#A44ED9",
+        "676767": "#676767",
+        "F9FAFB": "#F9FAFB",
+        "FF9029": "#FF9029",
+        "FF006B": "#FF006B",
+        "F1F3F6": "#F1F3F6",
+        "0E1028": "#0E1028",
+      }
+    },
+  },
+  plugins: [],
+}
