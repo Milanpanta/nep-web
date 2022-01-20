f228fd8d880047d0dfa9af990b97cb7ab0da4873
Milan Panta <milan@neptunemutual.com>
2022-01-20 06:49:14 +0545

    chore: create header component

--- components/utils/Header/index.jsx
+++ components/utils/Header/index.jsx
@@ -1,5 +1,5 @@
-// import React, { Fragment, ReactNode } from "react";
-// import { Popover, Transition } from "@headlessui/react";
+import React from "react";
+import { Popover } from "@headlessui/react";
 
 import { pageRoutes } from "@lib/routes";
 
@@ -20,3 +20,45 @@ const navlinks = [
     navKey: "contact",
   },
 ];
+
+const DesktopHref = ({ name, href, navKey, selectedNavKey }) => {
+  return (
+    <div className="">
+      <a
+        href={href}
+        className={`inline-flex items-center font-medium border-black hover:text-black hover:text-opacity-60 capitalize ${
+          selectedNavKey === navKey && "text-black"
+        } text-center`}
+      >
+        {name}
+      </a>
+    </div>
+  );
+};
+
+export const Header = ({ selectedNavKey }) => {
+  return (
+    <Popover className="sticky top-0 w-full z-30 bg-white h-auto shadow-md">
+      <div className="flex w-full justify-between gap-8 items-center py-2">
+        <div>
+          <a href={pageRoutes.home}></a>
+        </div>
+        <Popover.Group
+          as="nav"
+          className="hidden lg:flex flex-wrap gap-6 items-center justify-end text-gray-800 text-sm 2xl:text-base"
+        >
+          {navlinks.map(({ label, navKey, href }, i) => (
+            <React.Fragment key={i}>
+              <DesktopHref
+                href={href}
+                name={label}
+                navKey={navKey}
+                selectedNavKey={selectedNavKey}
+              />
+            </React.Fragment>
+          ))}
+        </Popover.Group>
+      </div>
+    </Popover>
+  );
+};
--- layouts/Base/index.jsx
+++ layouts/Base/index.jsx
@@ -1,5 +1,9 @@
-export const BaseLayout = ({ children }) => (
+import { Header } from "@components/utils/Header";
+
+export const BaseLayout = ({ children, selectedNavKey }) => (
   <div className="relative">
+    <Header selectedNavKey={selectedNavKey} />
     <main>{children}</main>
+    {/* <footer></footer> */}
   </div>
 );
