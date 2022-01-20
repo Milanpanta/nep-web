ef97e09949d3572115aae6eeea8b247d07cedc6a
Milan Panta <milan@neptunemutual.com>
2022-01-20 06:35:52 +0545

    chore: add headlessui and jsconfig

---
+++ components/utils/Header/index.jsx
@@ -0,0 +1,22 @@
+// import React, { Fragment, ReactNode } from "react";
+// import { Popover, Transition } from "@headlessui/react";
+
+import { pageRoutes } from "@lib/routes";
+
+const navlinks = [
+  {
+    href: pageRoutes.home,
+    label: "Home",
+    navKey: "home",
+  },
+  {
+    href: pageRoutes.blog,
+    label: "Blog",
+    navKey: "blog",
+  },
+  {
+    href: pageRoutes.contact,
+    label: "Contact",
+    navKey: "contact",
+  },
+];
