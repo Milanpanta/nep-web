0d98405300988033ac370270fd74f7dc5e712629
Milan Panta <milan@neptunemutual.com>
2022-01-21 16:48:35 +0545

    feat: create timeline component design

--- components/HomePage/Roadmap.jsx
+++ components/HomePage/Roadmap.jsx
@@ -1,3 +1,6 @@
+import ChevronRightIcon from "utils/icons/ChevronRightIcon";
+import ChevronLeftIcon from "utils/icons/ChevronLeftIcon";
+
 export const RoadmapComponent = () => {
   return (
     <div className="mx-72 px-26 pt-76">
@@ -8,8 +11,60 @@ export const RoadmapComponent = () => {
         Follow our progress, join our community
       </p>
 
-      <div className="mt-24">
-        <div></div>
+      <div className="mt-24 mb-14">
+        <div className="relative flex justify-between items-center">
+          <div className="w-14 h-14 border border-black rounded-full flex justify-center items-center z-30 bg-white">
+            <ChevronLeftIcon className="w-7 h-7" />
+          </div>
+          <div className="w-14 h-14 border border-black rounded-full flex justify-center items-center z-30 bg-white">
+            <ChevronRightIcon className="w-7 h-7" />
+          </div>
+          <div className="absolute w-full h-px bg-black">
+            <div className="flex justify-evenly relative -top-3">
+              <div className="bg-0E1028 w-7 h-7 rounded-full">
+                <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+                  Ideation
+                </div>
+              </div>
+              <div className="bg-4E7DD9 w-7 h-7 rounded-full">
+                <div className="relative text-4E7DD9 top-14 right-5 font-poppins tracking-wide">
+                  Product Design
+                </div>
+              </div>
+              <div className="bg-0E1028 w-7 h-7 rounded-full">
+                <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+                  Token Design
+                </div>
+              </div>
+              <div className="bg-0E1028 w-7 h-7 rounded-full">
+                <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+                  Product MVP
+                </div>
+              </div>
+              <div className="bg-0E1028 w-7 h-7 rounded-full">
+                <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+                  Cover SDK Beta
+                </div>
+              </div>
+              <div className="bg-0E1028 w-7 h-7 rounded-full">
+                <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+                  Incorporation & Legal
+                </div>
+              </div>
+              <div className="bg-4E7DD9 bg-opacity-10  w-20 h-20 rounded-full relative -top-7">
+                <div className="bg-4E7DD9 bg-opacity-20 w-16 h-16 rounded-full relative top-2 left-2">
+                  <div className="bg-4E7DD9 w-11 h-11 rounded-full relative top-2.5 left-2.5">
+                    <div className="bg-black w-7 h-7 rounded-full relative top-2 left-2">
+                      <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+                        Fund Raising, Recruitment & Build
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ utils/icons/ChevronLeftIcon.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const ChevronLeftIcon = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+  </svg>
+);
+
+export default ChevronLeftIcon;
