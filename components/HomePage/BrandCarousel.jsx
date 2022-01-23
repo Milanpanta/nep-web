83f8340ca0286ce17ff1b3846061de9c6d14a8b4
Milan Panta <milan@neptunemutual.com>
2022-01-23 14:16:02 +0545

    chore: make responsive brand carousel component

--- components/HomePage/BrandCarousel.jsx
+++ components/HomePage/BrandCarousel.jsx
@@ -6,17 +6,17 @@ import OkexImg from "@assets/image/brand/okex.png";
 
 export const BrandCarouselComponent = () => {
   return (
-    <div className="grid grid-cols-4 gap-5 mt-5">
-      <div className="shadow-brand text-center bg-white py-10">
+    <div className="grid grid-cols-2 xl:grid-cols-4 gap-5 mt-8 xl:mt-5">
+      <div className="shadow-brand text-center flex justify-center items-center bg-white px-4 py-6 xl:py-10">
         <Image src={CoinbaseImg} alt="Coinbase Image" />
       </div>
-      <div className="shadow-brand text-center bg-white py-10">
+      <div className="shadow-brand text-center flex justify-center items-center bg-white px-4 py-6 xl:py-10">
         <Image src={GravityImg} alt="Gravity Image" />
       </div>
-      <div className="shadow-brand text-center bg-white py-10">
+      <div className="shadow-brand text-center flex justify-center items-center bg-white px-4 py-6 xl:py-10">
         <Image src={HuobiImg} alt="Huobi Image" />
       </div>
-      <div className="shadow-brand text-center bg-white py-10">
+      <div className="shadow-brand text-center flex justify-center items-center bg-white px-4 py-6 xl:py-10">
         <Image src={OkexImg} alt="Okex Image" />
       </div>
     </div>
