29760a6cf2bdc546d1f38916f3525c3fee0ec0be
Milan Panta <milan@neptunemutual.com>
2022-01-23 19:25:02 +0545

    chore: make responsive timeline component

--- components/HomePage/Roadmap.jsx
+++ components/HomePage/Roadmap.jsx
@@ -1,54 +1,78 @@
 import ChevronRightIcon from "utils/icons/ChevronRightIcon";
 import ChevronLeftIcon from "utils/icons/ChevronLeftIcon";
 
+const data = [
+  {
+    name: "Ideation",
+  },
+  {
+    name: "Product Design",
+  },
+  {
+    name: "Token Design",
+  },
+  {
+    name: "Product MVP",
+  },
+  {
+    name: "Cover SDK Beta",
+  },
+  {
+    name: "Incorporation & Legal",
+  },
+  {
+    name: "Fund Raising, Recruitment & Build",
+  },
+];
+
 export const RoadmapComponent = () => {
   return (
     <div className="px-4 sm:px-32 3xl:border-x 3xl:border-b0c4db 3xl:mx-72 3xl:px-26 pt-26 lg:pt-76">
-      <p className="lg:w-3/4 leading-12 lg:leading-normal m-auto font-sora text-3xxl lg:text-h3 font-semibold text-left lg:text-center">
+      <p className="lg:w-3/4 leading-12 lg:leading-normal m-auto font-sora text-3xxl lg:text-h3 font-semibold text-left sm:text-center">
         Our roadmap involves growing the Neptune Mutual ecosystem
       </p>
-      <p className="font-poppins font-light lg:text-2xl leading-10 mt-2 lg:mt-4 text-center tracking-wide">
+      <p className="font-poppins font-light lg:text-2xl leading-10 mt-2 lg:mt-4 text-left sm:text-center tracking-wide">
         Follow our progress, join our community
       </p>
 
       {/* TimeLine  */}
-      <div className="mt-24">
+      <div className="mt-14 lg:mt-24">
         <div className="relative flex justify-between items-center">
-          <div className="w-14 h-14 border border-black rounded-full flex justify-center items-center z-30 bg-white">
+          <div className="hidden lg:flex w-14 h-14 border border-black rounded-full justify-center items-center z-30 bg-white">
             <ChevronLeftIcon className="w-7 h-7" />
           </div>
-          <div className="w-14 h-14 border border-black rounded-full flex justify-center items-center z-30 bg-white">
+          <div className="hidden lg:flex w-14 h-14 border border-black rounded-full justify-center items-center z-30 bg-white">
             <ChevronRightIcon className="w-7 h-7" />
           </div>
-          <div className="absolute w-full h-px bg-black">
+          <div className="absolute w-full h-px lg:bg-black bg-gradient-to-r from-transparent via-01052D to-transparent">
             <div className="flex justify-evenly relative -top-3">
-              <div className="bg-0E1028 w-7 h-7 rounded-full">
-                <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+              <div className=" bg-0E1028 w-7 h-7 rounded-full">
+                <div className="hidden lg:block relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
                   Ideation
                 </div>
               </div>
-              <div className="bg-4E7DD9 w-7 h-7 rounded-full">
+              <div className="hidden lg:flex bg-4E7DD9 w-7 h-7 rounded-full">
                 <div className="relative text-4E7DD9 top-14 right-5 font-poppins tracking-wide">
                   Product Design
                 </div>
               </div>
-              <div className="bg-0E1028 w-7 h-7 rounded-full">
+              <div className="hidden lg:flex bg-0E1028 w-7 h-7 rounded-full">
                 <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
                   Token Design
                 </div>
               </div>
-              <div className="bg-0E1028 w-7 h-7 rounded-full">
+              <div className="hidden lg:flex bg-0E1028 w-7 h-7 rounded-full">
                 <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
                   Product MVP
                 </div>
               </div>
-              <div className="bg-0E1028 w-7 h-7 rounded-full">
-                <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+              <div className="hidden lg:flex bg-0E1028 w-7 h-7 rounded-full">
+                <div className="hidden lg:block relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
                   Cover SDK Beta
                 </div>
               </div>
-              <div className="bg-0E1028 w-7 h-7 rounded-full">
-                <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+              <div className="hidden lg:flex bg-0E1028 w-7 h-7 rounded-full">
+                <div className=" relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
                   Incorporation & Legal
                 </div>
               </div>
@@ -56,11 +80,17 @@ export const RoadmapComponent = () => {
                 <div className="bg-4E7DD9 bg-opacity-20 w-16 h-16 rounded-full relative top-2 left-2">
                   <div className="bg-4E7DD9 w-11 h-11 rounded-full relative top-2.5 left-2.5">
                     <div className="bg-black w-7 h-7 rounded-full relative top-2 left-2">
-                      <div className="relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+                      <div className="hidden lg:block relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
                         Fund Raising, Recruitment & Build
                       </div>
                     </div>
                   </div>
+                </div>
+              </div>
+
+              <div className="lg:hidden bg-white border border-black w-7 h-7 rounded-full">
+                <div className="hidden lg:block relative top-14 right-5 font-poppins font-light opacity-50 tracking-wide">
+                  Cover SDK Beta
                 </div>
               </div>
             </div>
@@ -69,11 +99,11 @@ export const RoadmapComponent = () => {
       </div>
 
       {/* Description */}
-      <div className=" mt-44 text-left pl-36">
-        <p className="font-poppins font-semibold text-4xl leading-10">
+      <div className="mt-14 lg:mt-44 text-left lg:pl-36">
+        <p className="font-poppins font-semibold text-xl lg:text-4xl leading-8 lg:leading-10">
           Product Design
         </p>
-        <p className="w-1/2 font-poppins font-light mt-4">
+        <p className="lg:w-1/2 font-poppins font-light mt-4">
           The design of Neptune Mutual’s cover protocol is driven by four
           underlying principles, namely:
         </p>
