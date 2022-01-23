fb00519af9c8fb193416d0487f1db325440a0afb
Milan Panta <milan@neptunemutual.com>
2022-01-23 17:00:01 +0545

    chore: make responsive get covered component

--- components/HomePage/GotCovered.jsx
+++ components/HomePage/GotCovered.jsx
@@ -4,13 +4,13 @@ import CreateIcon from "@utils/icons/CreateIcon";
 
 export const GotCoveredComponent = () => {
   return (
-    <div className="w-screen h-screen px-72 bg-01052D overflow-x-hidden">
+    <div className="w-screen h-auto pb-18 sm:pb-46 3xl:px-72 bg-01052D overflow-x-hidden">
       <div className="relative">
         <div className="absolute -top-14">
           <img src="/cover-mask.png" alt="Cover Mask" className="" />
         </div>
-        <div className="pl-28 pr-26 pt-38">
-          <p className="font-sora w-1/2 font-semibold text-h2 text-transparent bg-clip-text bg-gradient-to-l from-3C69FE to-E86BFB">
+        <div className="px-4 sm:pl-28 sm:pr-26 pt-38">
+          <p className="font-sora lg:w-1/2 font-semibold text-h2 text-transparent bg-clip-text bg-gradient-to-l from-3C69FE to-E86BFB">
             We’ve Got You Covered
           </p>
           <p className="text-white font-poppins font-light text-2xl pt-4 leading-10">
@@ -19,8 +19,8 @@ export const GotCoveredComponent = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-3 gap-5 pl-28 pr-26 pt-16">
-          <div className="bg-CDDFFB bg-opacity-10 py-16 px-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-4 sm:pl-28 sm:pr-26 pt-16 ">
+          <div className="bg-CDDFFB bg-opacity-10 rounded-xl py-16 px-10">
             <StarIcon className="w-10 h-10" />
             <p className="pt-10 font-inter font-semibold text-2xl text-transparent bg-clip-text tracking-wide leading-7 bg-gradient-to-l from-3C69FE to-E86BFB">
               Earn Rewards
@@ -34,7 +34,7 @@ export const GotCoveredComponent = () => {
             </p>
           </div>
 
-          <div className="bg-CDDFFB bg-opacity-10 py-16 px-10">
+          <div className="bg-CDDFFB bg-opacity-10 rounded-xl py-16 px-10">
             <CautionIcon className="w-10 h-10" />
             <p className="pt-10 font-inter font-semibold text-2xl text-transparent bg-clip-text tracking-wide leading-7 bg-gradient-to-l from-3C69FE to-E86BFB">
               Get Protection
@@ -48,7 +48,7 @@ export const GotCoveredComponent = () => {
             </p>
           </div>
 
-          <div className="bg-CDDFFB bg-opacity-10 py-16 px-10">
+          <div className="bg-CDDFFB bg-opacity-10 rounded-xl py-16 px-10">
             <CreateIcon className="w-10 h-10" />
             <p className="pt-10 font-inter font-semibold text-2xl text-transparent bg-clip-text tracking-wide leading-7 bg-gradient-to-l from-3C69FE to-E86BFB">
               Create Covers
