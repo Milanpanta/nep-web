ef90d77f93c98b29b34698c4b88f25bd63312b99
Milan Panta <milan@neptunemutual.com>
2022-01-23 18:42:06 +0545

    chore: make responsive neptune ecosystem component

--- components/HomePage/NeptuneEcosystem.jsx
+++ components/HomePage/NeptuneEcosystem.jsx
@@ -5,29 +5,32 @@ import BottomRightImg from "@assets/image/ecosystem/bottom-right.png";
 
 export const NeptuneEcosystemComponent = () => {
   return (
-    <div className="bg-01052D relative pt-46 w-screen h-screen overflow-x-hidden">
-      <div className="absolute top-0 left-0">
+    <div className="bg-01052D relative pt-18 lg:pt-46 w-screen h-auto overflow-x-hidden">
+      <div className="hidden lg:block absolute top-0 left-0">
         <Image src={TopLeftImg} alt="Vector Image" />
       </div>
-      <div className="absolute bottom-0 left-0">
+      <div className="absolute w-52 sm:w-68 md:w-80 top-16 right-0 lg:hidden">
         <Image src={BottomLeftImg} alt="Vector Image" />
       </div>
-      <div className="absolute bottom-0 right-0">
+      <div className="hidden lg:block absolute lg:bottom-0 lg:left-0 3xl:bottom-0 3xl:left-56">
+        <Image src={BottomLeftImg} alt="Vector Image" />
+      </div>
+      <div className="hidden lg:block absolute bottom-0 right-0">
         <Image src={BottomRightImg} alt="Vector Image" />
       </div>
-      <div className="mx-72 px-26 flex justify-between gap-28">
+      <div className="px-4 sm:px-32 3xl:mx-72 3xl:px-26 pb-38 lg:pb-96 flex flex-col lg:flex-row lg:justify-between lg:gap-28">
         <div>
-          <p className="w-3/4 font-sora font-semibold text-h2 text-F9FAFB">
+          <p className="lg:w-3/4 font-sora font-semibold text-center sm:text-left text-3xxl lg:text-h2 text-F9FAFB">
             Neptune Mutual Ecosystem
           </p>
-          <p className="pt-2.5 font-poppins text-transparent text-4xxl leading-14 bg-clip-text bg-gradient-to-r from-FF9029 via-FF006B to-FF006B">
+          <p className="pt-20 lg:pt-2.5 font-poppins text-transparent text-2xl lg:text-4xxl leading-10 lg:leading-14 bg-clip-text bg-gradient-to-r from-FF9029 via-FF006B to-FF006B">
             Low Risk Cover Policies
           </p>
         </div>
 
-        <div>
+        <div className="mt-8 lg:mt-0">
           <div>
-            <p className="font-sora font-semibold text-transparent text-4xxl leading-10 bg-clip-text bg-gradient-to-r from-FF9029 via-FF006B to-FF006B">
+            <p className="font-sora font-semibold text-transparent text-lg lg:text-4xxl leading-6 lg:leading-10 bg-clip-text bg-gradient-to-r from-FF9029 via-FF006B to-FF006B">
               Zero Risk
             </p>
             <p className="pt-2 font-poppins font-light text-white leading-6">
@@ -35,7 +38,7 @@ export const NeptuneEcosystemComponent = () => {
             </p>
           </div>
           <div className="mt-10">
-            <p className="font-sora font-semibold text-transparent text-4xxl leading-10 bg-clip-text bg-gradient-to-r from-FF9029 via-FF006B to-FF006B">
+            <p className="font-sora font-semibold text-transparent text-lg lg:text-4xxl leading-6 lg:leading-10 bg-clip-text bg-gradient-to-r from-FF9029 via-FF006B to-FF006B">
               100%+ MCR
             </p>
             <p className="pt-2 font-poppins font-light text-white leading-6">
@@ -43,7 +46,7 @@ export const NeptuneEcosystemComponent = () => {
             </p>
           </div>
           <div className="mt-10">
-            <p className="font-sora font-semibold text-transparent text-4xxl leading-10 bg-clip-text bg-gradient-to-r from-FF9029 via-FF006B to-FF006B">
+            <p className="font-sora font-semibold text-transparent text-lg lg:text-4xxl leading-6 lg:leading-10 bg-clip-text bg-gradient-to-r from-FF9029 via-FF006B to-FF006B">
               Dedicated Cover Pools
             </p>
             <p className="pt-2 font-poppins font-light text-white leading-6">
@@ -51,7 +54,7 @@ export const NeptuneEcosystemComponent = () => {
             </p>
           </div>
           <div className="mt-10">
-            <p className="font-sora font-semibold text-transparent text-4xxl leading-10 bg-clip-text bg-gradient-to-r from-FF9029 via-FF006B to-FF006B">
+            <p className="font-sora font-semibold text-transparent text-lg lg:text-4xxl leading-6 lg:leading-10 bg-clip-text bg-gradient-to-r from-FF9029 via-FF006B to-FF006B">
               Stablecoin Liquidity
             </p>
             <p className="pt-2 font-poppins font-light text-white leading-6">
